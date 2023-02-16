2d79a7a8cb0db71d12c6e9dad64be44407cd8f91
neymaru <106804514+neymaru@users.noreply.github.com>
2023-02-16 22:38:37 +0900

    todo 리스트

---
+++ DOM/calendar/user2/main.js
@@ -0,0 +1,45 @@
+const input = document.querySelector(".inputBox");
+const addBtn = document.querySelector(".addBtn");
+const ul = document.querySelector(".ul");
+
+let targetEl; // 이벤트 target 을 저장할 전역 변수
+
+// 2. addListTask 함수 구현
+function addListTask(e) {
+  // 3. 내용 입력 안하면 placeholder 문구 띄우기
+  if (input.value === "") {
+    input.setAttribute("placeholder", "내용을 입력해주세욧");
+    return;
+  }
+
+  const li = document.createElement("li");
+  const checkBox = document.createElement("input");
+  const deleteBtn = document.createElement("button");
+
+  checkBox.setAttribute("type", "checkbox");
+  deleteBtn.textContent = "삭제";
+
+  // 4. checkbox 클릭하면 line-through 주기
+  checkBox.addEventListener("click", function () {
+    if (checkBox.checked === true) {
+      checkBox.parentNode.style.textDecoration = "line-through";
+    } else {
+      checkBox.parentNode.style.textDecoration = "none";
+    }
+  });
+
+  // 5. 삭제 버튼 누르면 li 삭제
+  deleteBtn.addEventListener("click", function (e) {
+    targetEl = e.target;
+    targetEl.parentNode.remove(); // button의 부모인 li 삭제
+  });
+
+  // 6. li 리스트 추가
+  li.append(checkBox);
+  li.append(input.value);
+  li.append(deleteBtn);
+  ul.append(li);
+}
+
+// 1. '추가' 버튼 누르면 addListTask 함수 실행
+addBtn.addEventListener("click", addListTask);
