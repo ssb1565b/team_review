299415fd0a9ff26fad3c23cbb47d66a3ae1878f3
ssb1565b <ssongsumovie@naver.com>
2023-02-28 22:39:20 +0900

    프로그래머스 문제3개

---
+++ Programmers/Lv0/120831_ssb.js
@@ -0,0 +1,16 @@
+// 짝수의 합
+// https://school.programmers.co.kr/learn/courses/30/lessons/120831
+
+// 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+
+function solution(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    if (i % 2 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(solution(4));
